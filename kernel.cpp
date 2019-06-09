98f20c092fa870d6a25c15c78f6a7d9a144dcdd4
Piyush Itankar <pitankar@gmail.com>
2019-06-09 20:07:10 +0530

    Adds a Global Descriptor Table
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,5 @@
 #include "types.h"
+#include "gdt.h"
 
 void printf(char *str){
     uint16_t *VideoMemory = (uint16_t*)0xb8000;
@@ -19,6 +20,8 @@ extern "C" void callConstructors() {
 
 extern "C" void kernelMain(void *multiboot_structure, uint32_t magicnumber){
     printf("Hello world!");
+
+    GlobalDescriptorTable gdt;
 
     while(1);
 }
