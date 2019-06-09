4c1268febcea971f724d9bcfe3a733024a0fa627
Piyush Itankar <pitankar@gmail.com>
2019-06-09 11:17:17 +0530

    Adds call to Constructor caller before kernelMain is triggered
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- kernel.cpp
+++ kernel.cpp
@@ -5,6 +5,16 @@ void printf(char *str){
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+
+extern "C" void callConstructors() {
+    for(constructor *i = &start_ctors; i != &end_ctors; i++) {
+        (*i)();
+    }
+}
+
 extern "C" void kernelMain(void *multiboot_structure, unsigned int magicnumber){
     printf("Hello world!");
 
--- loader.s
+++ loader.s
@@ -9,10 +9,12 @@
 
 .section .text
 .extern kernelMain
+.extern callConstructors
 .global loader
 
 loader:
     mov $kernel_stack, %esp
+    call callConstructors
     push %eax
     push %ebx
     call kernelMain
